9f1e7e49a7cd7e3d6bc7da5b05ee141fb8135b61
PlacidBeetle <beetleaverage78@gmail.com>
2019-03-26 18:31:37 +1300

    Refund

--- donut_v1.py
+++ donut_v1.py
@@ -2,43 +2,11 @@ progress = True
 
 CHOCOLATE_PRICE = float(2.50)
 CARAMEL_PRICE = float(2.50)
-CINNAMON_PRICE = float(1.20)
+CINNAMON_PRICCE = float(1.20)
 
 customer_num = 0
 
 def DonutQuery():
-    donut_count = []
-    donut_type = []
-    user_donut = 0
-    repeat = True
-    print("Donut Query Function (v1)")
-    print("---------------------------")
-    print("What types of Donut is to be ordered?")
-    print("Chocolate Donut[1] Caramel Donut[2] Cinnamon Donut[3]")
-    print("Pick the Donuts corresponding with their numbers, seperated by spaces")
-
-    while repeat == True : 
-        try:
-            donut_count = input("Donuts: ").split()
-            if donut_count in ['1', '2', '3']:
-                break
-                print("Success")
-            else:
-                raise ValueError
-            
-        except ValueError:
-            print("That is neither 1, 2 or 3")
-            
-        print(donut_count)
-        donut_count = []
-        
-    for donut in donut_count:
-        if donut == '1':
-            user_input = input("How many Chocolate Donuts? ($2.50 each): ")
-        elif donut == '2':
-            user_input = input("How many Caramel Donuts? ($2.50 each): ")
-        elif donut == '3':
-            user_input = input("How many Cinnamon Donuts? ($1.20 each: )")
     
     
 def CalculateQuery():
@@ -59,7 +27,7 @@ def InputQuery(input_details, customer_num):
         elif i == 2:
             user_input = input("Customer's Phone Number: ")
         elif i == 3:
-            user_input = DonutQuery()
+            user_input = Donut_Query()
         elif i == 4:
             break
         
@@ -74,7 +42,7 @@ input_details, customer_num = InputQuery(input_details, customer_num)
 
 print(input_details)
 print("customer_num, date_order, fname, lname, customer_phone, donut_order")
-print("Fname and Lname is {}".format(input_details[2]))
+print("Fname and Lnam is {}".format(input_details[2]))
 
 
 
