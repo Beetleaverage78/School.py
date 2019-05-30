d739b72666cf35f034ad38fd2aa9a4eb26b6af9e
PlacidBeetle <22809560+Beetleaverage78@users.noreply.github.com>
2019-05-31 09:19:37 +1200

    rwasr

--- donut_gui.py
+++ donut_gui.py
@@ -1,36 +1,18 @@
 import time 
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox as msg
 
 CHOCOLATE_PRICE = float(2.50)
 CARAMEL_PRICE = float(2.50)
 CINNAMON_PRICE = float(1.20)
 
-class process_orders():
 
-    def __init__(self, master, *args, **kwargs):
-        self.master = master
-        self.master.geometry('400x200+100+200')
-        self.master.title('Order Details')
-
-        self.init_window2()
-        
-    def write(self):
-        print("TestRun")
-        
-    def init_window2(self):
-        self.button1 = ttk.Button(self.master, text='Yes', command=self.write)
-        self.button1.grid(row=6, column=4, columnspan=2)
-
-        self.button1 = ttk.Button(self.master, text='No', command=self.write)
-        self.button1.grid(row=6, column=8, columnspan=2)
-        
-
-        
-            
+          
         
 class DonutCalculator(ttk.Frame):
-
+    global info
+    info = "No action"
     def __init__(self, master, goodbye, *args, **kwargs):
         ttk.Frame.__init__(self, master, *args, **kwargs)
         self.root = master
@@ -38,8 +20,7 @@ class DonutCalculator(ttk.Frame):
         self.goodbye = "Goodbye"
 
     def process(self):
-        orders = tkinter.Toplevel(self.master)
-        gui2 = process_orders(orders, "")
+        msg.showinfo("Test", "Some Message Box")
     
     def exit_prog(self):
         """Exits program."""
@@ -54,6 +35,7 @@ class DonutCalculator(ttk.Frame):
                 donut1 = int(self.donut1_entry.get())
                 donut2 = int(self.donut2_entry.get())
                 donut3 = int(self.donut3_entry.get())
+                print(donut1)
                 if (donut1 >=5 and donut2 >= 5 and donut3 >= 5) and (donut1 < 30 and donut2 < 30 and donut3 < 30):
                     sub_total1 = donut1 * CHOCOLATE_PRICE
                     sub_total2 = donut2 * CARAMEL_PRICE
@@ -73,6 +55,7 @@ class DonutCalculator(ttk.Frame):
                 print("Ready to enter another number if needed")
                 break
             finally:
+                info = "Test"
                 print("Read the reminder message")
                 break
  
@@ -83,24 +66,20 @@ class DonutCalculator(ttk.Frame):
         
         self.root.title('Donut Order Calculator')
         self.root.option_add('*tearOff', 'FALSE')
- 
         self.grid(column=0, row=0, sticky='nsew')
- 
+
+        # Menu ----------------------------------------
         self.menubar = tkinter.Menu(self.root)
- 
         self.menu_file = tkinter.Menu(self.menubar)
         self.menu_file.add_command(label='Exit', command=self.exit_prog)
-
- 
         self.menubar.add_cascade(menu=self.menu_file, label='File')
- 
         self.root.config(menu=self.menubar)
+        # ---------------------------------------------
 
         # Separator helps order the info on the GUI -------------------------------------
         ttk.Separator(self, orient='horizontal').grid(column=0,row=2, columnspan=8, sticky='we')
         ttk.Separator(self, orient='vertical').grid(column=1,row=2, rowspan=4, sticky='ns')
         ttk.Separator(self, orient='vertical').grid(column=3,row=2, rowspan=4, sticky='ns')
-        ttk.Separator(self, orient='horizontal').grid(column=0,row=4, columnspan=8, sticky='we')
         # ttk.Separator(self, orient='horizontal').grid(column=0,row=6, columnspan=8, sticky='we')
         # -------------------------------------------------------------------------------
 
@@ -118,13 +97,13 @@ class DonutCalculator(ttk.Frame):
         self.message_frame = ttk.LabelFrame(self, text='Output',height=100)
         self.message_frame.grid(column=0, row=7, columnspan=8, sticky='nesw')
  
-        self.message_label = ttk.Label(self.message_frame, text='Please enter a whole number from 5 to 30 only')
+        self.message_label = ttk.Label(self.message_frame, text=info)
         self.message_label.grid(column=0, row=0)
         # -------------------------------------------------------------------------------
 
         # Chocolate Donut Input ---------------------------------------------------------
-        self.donut1_entry = ttk.Entry(self, width=5)
-        self.donut1_entry.insert(0, 0)
+        default = tkinter.DoubleVar(value=0)
+        self.donut1_entry = tkinter.Spinbox(self, from_=0, to=30, textvariable=default, state='readonly')
         self.donut1_entry.grid(column=0, row=4)
         
         self.sub_total_frame1 = ttk.LabelFrame(self, text='Sub Total',height=100)
@@ -182,6 +161,7 @@ class DonutCalculator(ttk.Frame):
  
 if __name__ == '__main__':
     root = tkinter.Tk()
+    tkinter.Label(root, text="First").grid(row=0)
     gui1 = DonutCalculator(root, "")
     root.mainloop()
     
