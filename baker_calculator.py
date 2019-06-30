a01e6ebac74e5f9c6a777dbd2e31467e9cd2bf0f
PlacidBeetle <beetleaverage78@gmail.com>
2019-07-01 07:24:10 +1200

    new

--- baker_calculator.py
+++ baker_calculator.py
@@ -1,38 +1,86 @@
-cookie_max = 10
-cupcake_max = 10
-cake_max = 5
-
-
+from tkinter import messagebox as mg
 def start():
     print("Baker Method Imported Successfully")
 
-def calculate_method(order, price, count):
-    print("------------------------------------------------------")
-    print("Order Requested - Checking Maximum and Minimum orders")
-    print("------------------------------------------------------")
+def name_method(fname, lname):
+    judge = 'missing'
+    f_count = 0
+    l_count = 0
+    if fname == "" and lname != "":
+        msg = "Missing Customer's First Name"
+        return judge, msg
+    elif lname == "" and fname != "":
+        msg = "Missing Customer's Last Name"
+        return judge, msg
+    elif fname == "" and lname == "":
+        msg = "Missing Customer's First & Last Name"
+        return judge, msg
+    mg.showinfo('Processing', "Processing Customer Details")
+    print("-------Name Checking Intensifies----------")
+    print("Checking Length of name is appropriate")
+    if len(fname) < 2:
+        judge = False
+        msg = "First Name is too short, needs to be 2 or more characters long"
+        return judge, msg
+    elif len(lname) < 2:
+        judge = False
+        msg = "Last Name is too short, needs to be 2 or more characters long"
+        return judge, msg
+    
+    print("Checking for Spaces")
+    for letter in fname:
+        if (letter.isspace()) == True:
+            f_count += 1
+    for letter in lname:
+        if (letter.isspace()) == True:
+            l_count += 1
+    if f_count > 0 and l_count > 0:
+        judge = False
+        msg = "There are spaces in both the First & Last Name of the customer"
+        return judge, msg
+    elif f_count > 0:
+        judge = False
+        msg = "There are spaces in the First Name of the customer"
+        return judge, msg
+    elif l_count > 0:
+        judge = False
+        msg = "There are spaces in the Last Name of the customer"
+        return judge, msg
+    
+    else:
+        print("Checking if name is in the alphabet")
+        if fname.isalpha() == True and lname.isalpha() == True:
+            judge = True
+            msg = "Valid Customer Details"
+            print("-----Successful Name Checking------")
+            return judge, msg
+        elif fname.isalpha() == False and lname.isalpha() == False:
+            judge = False
+            msg = "First & Last Name is Invalid"
+            return judge, msg
+        elif fname.isdigit() or fname.isalpha() == False:
+            judge = False
+            msg = "First Name is Invalid"
+            return judge, msg
+        elif lname.isdigit() or lname.isalpha() == False:
+            judge = False
+            msg = "Last Name is Invalid"
+            return judge, msg
+
+
+def calculate_method(order, price, count, order_max):
     sub_total = 0
     count += order
-    if price != 59.50:
-        if count <= 0:
-            judge = False
-            return judge, sub_total
-        elif count > 10:
-            judge = False
-            sub_total = count * price
-            return judge, sub_total
-        elif count > 0:
-            judge = True
-            sub_total = count * price
-            return judge, sub_total
-    elif price == 59.50:
-        if count <= 0:
-            judge = False
-            return judge, sub_total
-        elif count > 5:
-            judge = False
-            sub_total = count * price
-            return judge, sub_total
-        elif count > 0:
-            judge = True
-            sub_total = count * price
-            return judge, sub_total
+    if count <= 0:
+        judge = False
+        return judge, sub_total
+    elif count > order_max:
+        judge = False
+        sub_total = count * price
+        return judge, sub_total
+    elif count > 0:
+        judge = True
+        sub_total = count * price
+        print("-----Successful Calculation----")
+        return judge, sub_total
+        
--- bakergui_version2.py
+++ bakergui_version2.py
@@ -8,6 +8,10 @@ COOKIES_PRICE = float(16.20)
 CUPCAKES_PRICE = float(21)
 CAKE_PRICE = float(59.50)
 
+COOKIES_MAX = 10
+CUPCAKES_MAX = 10
+CAKE_MAX = 5
+
 date = dt.datetime.now()
 bc.start()
 
@@ -38,7 +42,7 @@ class DonutGui(tk.Frame):
         var3.set(0)
 
     def reset(self):
-        check = mg.askyesno("Reset", "Do you want to reset the program?")
+        check = mg.askyesno("Reset", "Do you want to reset the program")
         if check == True:
             self.init_window()
         else:
@@ -47,6 +51,33 @@ class DonutGui(tk.Frame):
     def exit(self):
         quit()
 
+    def process_orders(self):
+        check = mg.askyesno('Final Orders?', "Is this the final order?")
+        if check == True:
+            pass
+        else:
+            return
+        while True:
+            fname = self.fname_entry.get()
+            lname = self.lname_entry.get()
+            judge, msg = bc.name_method(fname, lname)
+            if judge == True:
+                mg.showinfo('Successful', msg)
+                break
+            elif judge == False:
+                mg.showerror('Invalid Name', msg)
+                return
+            else:
+                mg.showerror('Missing Values', msg)
+                return
+        orders = {}
+        orders['Cookies'] = [cookies_count, round(cookies_total)]
+        orders['Cupcakes'] = [cupcakes_count, round(cupcakes_total)]
+        orders['Cakes'] = [cakes_count, round(cakes_total)]
+        mg.showinfo('Order Details', orders)
+        mg.showinfo('Resetting', "Program will now reset to prepare for another order")
+        self.init_window()
+                             
     def calculate(self):
         global cookies_count, cookies_total, cupcakes_count, cupcakes_total, cakes_count, cakes_total, total_cost
         judge = 0
@@ -55,37 +86,38 @@ class DonutGui(tk.Frame):
         
         cookies = int(var1.get())
         if cookies != 0:
-            judge, total = bc.calculate_method(cookies, COOKIES_PRICE, cookies_count)
+            judge, total = bc.calculate_method(cookies, COOKIES_PRICE, cookies_count, COOKIES_MAX)
             if judge == True:
                 cookies_count += cookies
                 cookies_total += total
                 self.cookies_sub_label['text'] = "${:.2f}".format(cookies_total)
             else:
-                mg.showerror('Invalid Input', "That is more than 10 Cookie packs, Current: {}".format(cookies_count))
+                mg.showerror('Invalid Input', "That is more than 10 Cookie packs, You are trying to order {} more, When you have already {}".format(cookies, cookies_count))
         else:
             if cookies_count > 0:
                 self.cookies_sub_label['text'] = "${:.2f}".format(cookies_total)
         cupcakes = int(var2.get())
         if cupcakes != 0:
-            judge, total = bc.calculate_method(cupcakes, CUPCAKES_PRICE, cupcakes_count)
+            judge, total = bc.calculate_method(cupcakes, CUPCAKES_PRICE, cupcakes_count, CUPCAKES_MAX)
             if judge == True:
                 cupcakes_count += cupcakes
                 cupcakes_total += total
                 self.cupcakes_sub_label['text'] = "${:.2f}".format(cupcakes_total)
             else:
-                mg.showerror('Invalid Input', "That is more than 10 Cupcake packs, Current: {}".format(cupcakes_count))
+                mg.showerror('Invalid Input', "That is more than 10 Cupcake packs, You are trying to order {} more, When you have already ordered {}".format(cupcakes, cupcakes_count))
         else:
             if cupcakes > 0:
                 self.cupcakes_sub_label['text'] = "${:.2f}".format(cupcakes_total)
         cakes = int(var3.get())
         if cakes != 0:
-            judge, total = bc.calculate_method(cakes, CAKE_PRICE, cakes_count)
+            judge, total = bc.calculate_method(cakes, CAKE_PRICE, cakes_count, CAKE_MAX)
             if judge == True:
                 cakes_count += cakes
                 cakes_total += total
                 self.cakes_sub_label['text'] = "${:.2f}".format(cakes_total)
+                self.cake_label['text'] = "Cakes: {}".format(cakes)
             else:
-                mg.showerror('Invalid Input', "That is more than 5 Cakes, Current: {}".format(cakes_count))
+                mg.showerror('Invalid Input', "That is more than 5 Cakes, You are trying to order {} more, When you have already ordered {}".format(cakes, cakes_count))
         else:
             if cakes > 0:
                 self.cakes_sub_label['text'] = "${:.2f}".format(cakes_total)
@@ -98,7 +130,9 @@ class DonutGui(tk.Frame):
             self.process_button['state'] = 'normal'
             total_cost = cookies_total + cupcakes_total + cakes_total + 10
             self.total_label['text'] = "${:.2f}".format(total_cost)
+            self.output_frame['text'] = "Output: Successful Calculation"
         else:
+            self.output_frame['text'] = "Output: No Orders were made"
             mg.showinfo("Zero Orders", "No Orders were made")
             
     def init_window(self):
@@ -117,37 +151,43 @@ class DonutGui(tk.Frame):
         ttk.Separator(self, orient='horizontal').grid(column=0, row=7, columnspan=9, sticky='we')
         # --------------------------------------------------------------------------------------
 
-        # Middle Buttons --------------------------------------------------------
-        self.process_button = tk.Button(self, text='Process Orders', state='disabled')
-        self.process_button.grid(column=0, row=9, columnspan=1, sticky='we')
-
-        self.calculate_button = tk.Button(self, text='Calculate', command=self.calculate)
-        self.calculate_button.grid(column=1, row=9, columnspan=3, sticky='nesw')
-
+        # Main Buttons --------------------------------------------------------
         self.reset_button = tk.Button(self, text='Reset', command=self.reset)
-        self.reset_button.grid(column=4, row=9, sticky='we')
+        self.reset_button.grid(column=2, row=1, columnspan=1, sticky='we')
         
         self.exit_button = tk.Button(self, text='Exit', command=self.exit)
-        self.exit_button.grid(column=4, row=14, columnspan=1, sticky='we')
+        self.exit_button.grid(column=5, row=1, columnspan=1, sticky='we')
+        
+        self.process_button = tk.Button(self, text='Process Orders', state='disabled', command=self.process_orders)
+        self.process_button.grid(column=0, row=9, sticky='we')
+
+        self.calculate_button = tk.Button(self, text='Calculate', command=self.calculate)
+        self.calculate_button.grid(column=1, row=9, columnspan=5, sticky='we')
         # -----------------------------------------------------------------------
         
         # Output & Total Cost ----------------------------------------------
-        self.output_frame = tk.LabelFrame(self, text='Output', bd='2px')
+        self.output_frame = tk.LabelFrame(self, text='Output: No Orders were Made', bd='2px')
         self.output_frame.grid(column=0, row=10, columnspan=8, sticky='nesw')
 
-        self.output_label = tk.Label(self.output_frame, text='None')
-        self.output_label.grid(column=0, row=0)
+        self.cookie_pack_label = tk.Label(self.output_frame, text='Cookie Packs: 0')
+        self.cookie_pack_label.grid(column=0, row=1, sticky='w')
+
+        self.cupcake_pack_label = tk.Label(self.output_frame, text='Cupcake Packs: 0')
+        self.cupcake_pack_label.grid(column=1, columnspan=2, row=1, sticky='w')
+
+        self.cake_label = tk.Label(self.output_frame, text='Cakes: 0')
+        self.cake_label.grid(column=4, row=1, sticky='we')
 
         self.total_frame = tk.LabelFrame(self, text='Total Cost', bd='2px')
         self.total_frame.grid(column=0, row=13, columnspan=8, sticky='nesw')
         
-        self.total_label = tk.Label(self.total_frame, text='None')
+        self.total_label = tk.Label(self.total_frame, text='$0.00')
         self.total_label.grid(column=0, row=0)
         # ------------------------------------------------------------------
 
         # Customer Details ------------------------------------------------------------
         self.customer_frame = tk.LabelFrame(self, text='Customer Details', bd='2px', height=100)
-        self.customer_frame.grid(column=0, row=1, columnspan=5, sticky='esw')
+        self.customer_frame.grid(column=0, row=1, columnspan=1, sticky='esw')
 
         self.fname_label = tk.Label(self.customer_frame, text='First Name')
         self.fname_label.grid(column=1, row=1)
@@ -164,43 +204,43 @@ class DonutGui(tk.Frame):
 
         # Cookies Input ---------------------------------------------------------------
         self.cookies_entry = tk.OptionMenu(self, var1, *__order_quantity)
-        self.cookies_entry.grid(column=1, row=4, columnspan=3, sticky='we')
+        self.cookies_entry.grid(column=1, row=4, columnspan=4, sticky='we')
         
         self.cookies_sub_frame = tk.LabelFrame(self, text='Sub Total')
-        self.cookies_sub_frame.grid(column=4, row=4, sticky='we')
+        self.cookies_sub_frame.grid(column=5, row=4, sticky='we')
 
-        self.cookies_sub_label = tk.Label(self.cookies_sub_frame, text='None')
+        self.cookies_sub_label = tk.Label(self.cookies_sub_frame, text='$0.00')
         self.cookies_sub_label.grid(column=0, row=0)
         # -----------------------------------------------------------------------------
 
         # Cupcakes Input --------------------------------------------------------------
         self.cupcakes_entry = tk.OptionMenu(self, var2, *__order_quantity)
-        self.cupcakes_entry.grid(column=1, row=5, columnspan=3, sticky='we')
+        self.cupcakes_entry.grid(column=1, row=5, columnspan=4, sticky='we')
         
         self.cupcakes_sub_frame = tk.LabelFrame(self, text='Sub Total')
-        self.cupcakes_sub_frame.grid(column=4, row=5, columnspan=1, sticky='we')
+        self.cupcakes_sub_frame.grid(column=5, row=5, columnspan=1, sticky='we')
 
-        self.cupcakes_sub_label = tk.Label(self.cupcakes_sub_frame, text='None')
+        self.cupcakes_sub_label = tk.Label(self.cupcakes_sub_frame, text='$0.00')
         self.cupcakes_sub_label.grid(column=0, row=0)
         # -----------------------------------------------------------------------------
 
         # Cakes Input -----------------------------------------------------------------
         self.cakes_entry = tk.OptionMenu(self, var3, *__cake_quantity)
-        self.cakes_entry.grid(column=1, row=6, columnspan=3, sticky='we')
+        self.cakes_entry.grid(column=1, row=6, columnspan=4, sticky='we')
         
         self.cakes_sub_frame = tk.LabelFrame(self, text='Sub Total', height=100)
-        self.cakes_sub_frame.grid(column=4, row=6, columnspan=1, sticky='we')
+        self.cakes_sub_frame.grid(column=5, row=6, columnspan=1, sticky='we')
 
-        self.cakes_sub_label = tk.Label(self.cakes_sub_frame, text='None')
+        self.cakes_sub_label = tk.Label(self.cakes_sub_frame, text='$0.00')
         self.cakes_sub_label.grid(column=0, row=0)
         # -----------------------------------------------------------------------------
 
         # Initialise Labels -----------------------------------------------------------------------------------------------------------
         tk.Label(self, text="{} {} {} - {}".format(date.strftime("%d"), date.strftime("%B"), date.strftime("%Y"), date.strftime("%I:%M %p"))).grid(column=0, row=0, columnspan=8)
         tk.Label(self, text='Max Order is [] - Minimum is []').grid(column=0, row=3, columnspan=8)
-        tk.Label(self, text='Cookie Pack of 12 - ${:.2f} each pack'.format(COOKIES_PRICE)).grid(column=0, row=4, columnspan=1)
-        tk.Label(self, text='Cupcake Pack of 6- ${:.2f} each pack'.format(CUPCAKES_PRICE)).grid(column=0, row=5, columnspan=1)
-        tk.Label(self, text='Single Cake - ${:.2f} each'.format(CAKE_PRICE)).grid(column=0, row=6, columnspan=1)
+        tk.Label(self, text='Cookie Pack of 12 - ${:.2f} each pack'.format(COOKIES_PRICE)).grid(column=0, row=4, columnspan=1, sticky='w')
+        tk.Label(self, text='Cupcake Pack of 6- ${:.2f} each pack'.format(CUPCAKES_PRICE)).grid(column=0, row=5, columnspan=1, sticky='w')
+        tk.Label(self, text='Single Cake - ${:.2f} each'.format(CAKE_PRICE)).grid(column=0, row=6, columnspan=1, sticky='w')
         # -----------------------------------------------------------------------------------------------------------------------------
 
         for child in self.winfo_children():
